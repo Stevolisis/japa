c741bd95bceffa03a2a35dfa53e36a6a431c6d51
Steven Joseph <stevolisisjoseph@gmail.com>
2023-10-24 13:05:39 +0100

    Login Page in progress

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-bgPrimary p-7`}>
+
+        <header className='pb-5'>
+          <h1 className='text-bgSecondary text-2xl font-extrabold'>JAPA</h1>
+        </header>
+
+          {children}
+
+      </body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-
+      colors:{
+        bgPrimary:'#ffffff',
+        bgSecondary:'#8030ec',
+        bgBlue:'#a5e9ff',
+        bgOrange:'#fee5a5',
+        bgRed:'#faa6a4',
+        txtPrimary:'#444444',
+        txtRed:'#d39d9f',
+        txtinput:'#c7c7c7',
+        txtLinks:'#ab91e5'
+      }
     },
   },
   plugins: [],
